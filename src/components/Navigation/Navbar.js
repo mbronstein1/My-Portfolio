987ec09687a93e5d40fb78d40c0b97546d7dc788
Matthew Bronstein <mbronstein1@gmail.com>
2022-11-01 19:29:56 -0500

    adjusted navbar and hamburger and added background image for body

--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -4,16 +4,35 @@ import './Navbar.css'
 
 
 export default function Navbar() {
-    const navLinks = ["About Me", "Projects", "Contact Me", "Resume"];
+    const navLinks = [
+        {
+            name: 'About Me',
+            link: 'about',
+        },
+        {
+            name: 'Portfolio',
+            link: 'portfolio',
+        },
+        {
+            name: 'Contact Me',
+            link: 'contact',
+        },
+         {
+            name: 'Resume',
+            link: 'resume'
+         }
+        ];
     return (
         <nav className="navbar navbar-expand-lg align-self-end mb-2 custom-nav">
             <div className="container-fluid">
                 <button className="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon">
+                        {/* <i class="fas fa-bars custom-hamburger"></i> */}
+                    </span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
+                <div className="collapse navbar-collapse " id="navbarNav">
                     <ul className="navbar-nav">
-                        {navLinks.map((item, index) => <li key={`${item}-${index}`} className="nav-item"><a className="nav-link nav-btn" href={`/${item}`}>{item}</a></li>)}
+                        {navLinks.map((item, index) => <li key={`${item.name}-${index}`} className="nav-item"><a className="nav-link nav-btn" href={`#${item.link}`}>{item.name}</a></li>)}
                     </ul>
                 </div>
             </div>
