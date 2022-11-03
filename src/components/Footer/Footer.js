7621bad3a53b1b47255c464391abfb3908621308
Matthew Bronstein <mbronstein1@gmail.com>
2022-11-03 00:53:23 -0500

    fixed link to LinkedIn profile

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
         <footer className="footer d-flex">
             <div>
                 <a href='https://github.com/mbronstein1' target='_blank' rel="noreferrer"><i className="fa-brands fa-github fa-xl px-3"></i></a>
-                <a href='www.linkedin.com/in/matthew-bronstein-a9230462' target='_blank' rel="noreferrer"><i className="fa-brands fa-linkedin fa-xl"></i></a>
+                <a href='https://www.linkedin.com/in/matthew-bronstein-a9230462' target='_blank' rel="noreferrer"><i className="fa-brands fa-linkedin fa-xl"></i></a>
                 <a href='https://www.facebook.com/bronstein007' target='_blank' rel="noreferrer"><i class="fa-brands fa-square-facebook fa-xl px-3"></i></a>
             </div>
             <div className='footer-copyright'>
