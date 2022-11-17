eb94421c805d56c55c45943bdca08143f0006beb
Matthew Bronstein <mbronstein1@gmail.com>
2022-11-17 16:43:21 -0600

    more experimenting w/ sizing

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Resume from './pages/Resume/Resume'
 
 export default function App() {
   return (
-    <div className="app">
+    <div className="app pb-1">
       <Router>
         <Header />
 
