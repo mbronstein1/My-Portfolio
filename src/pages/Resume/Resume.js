cc366b49bea2c5fdb96cb2aedf7a9649b4fd7a76
Matthew Bronstein <mbronstein1@gmail.com>
2022-12-02 16:04:24 -0600

    updated resume

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -1,5 +1,5 @@
 import React from "react";
-import webDevResume from '../../assets/Web-development-resume_V2.pdf'
+import webDevResume from '../../assets/Web-dev-resume_Final.pdf'
 import './Resume.css';
 
 export default function Resume() {
